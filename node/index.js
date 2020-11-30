d26a528524f47dc990dc0a60dd73f4f7e613da38
Danush Kumar <52857686+DK2K00@users.noreply.github.com>
2020-11-30 22:49:34 +0530

    Node stuff

---
+++ node/index.js
@@ -0,0 +1,6 @@
+const math = require('./math');
+console.log(math.cube(2));
+
+const colors = require("colors");
+const name = "This is done with help of colors package";
+console.log(name.rainbow);
---
+++ node/math.js
@@ -0,0 +1,9 @@
+const add = (x,y) => x + y;
+const cube = x => x*x*x;
+
+const math = {
+    add: add,
+    cube: cube
+};
+
+module.exports = math;
