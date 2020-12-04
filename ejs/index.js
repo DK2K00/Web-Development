7c03bd8741fd8d9e19662860cc92e89cd2e632d6
Danush Kumar <52857686+DK2K00@users.noreply.github.com>
2020-12-04 13:03:46 +0530

    Random stuff using EJS

--- ejs/index.js
+++ ejs/index.js
@@ -4,8 +4,9 @@ const path = require('path');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
-
 app.set('partials', path.join(__dirname, '/partials'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req,res) =>{
     res.render("home");
